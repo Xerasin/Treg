3325c05aefd50c7680cf99386d24413e8c6b17e5
Xerasin <phoenixteam20@gmail.com>
2013-11-13 18:29:52 -0800

    Ugh

---
+++ Rainbow_Cube/bin/Debug/Resources/shaders/basicrender.fs
@@ -0,0 +1,27 @@
+#version 330 core
+precision highp float;
+invariant in vec2 in_UV;
+invariant in vec4 in_Color;
+invariant in vec4 position;
+
+uniform sampler2D ngl_texture0;
+
+
+layout(location = 0) out vec4 color;
+
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+
+void main()
+{
+	
+	//vec4 col = mix(vec4(1.0, 0.0, 0.0, 1.0), vec4(0.0,1.0,0.0, 1.0), abs(position.x));
+	//color = col;
+	vec4 col = vec4(hsv2rgb(vec3(abs(position.x), 1.0, 1.0)), 1.0);
+	color = (col + texture2D( ngl_texture0, in_UV.xy)) * 0.5;
+}
---
+++ Rainbow_Cube/bin/Debug/Resources/shaders/basicrender.vs
@@ -0,0 +1,21 @@
+#version 330 core
+precision highp float;
+layout( location = 0 ) in vec3 in_Position;
+layout( location = 1 ) in vec3 Normal;
+layout( location = 2 ) in vec2 vertexUV;
+layout( location = 3 ) in vec4 dgl_Color;
+uniform mat4 ModelMatrix;
+uniform mat4 ProjectionMatrix;
+uniform mat4 ViewMatrix;
+uniform float time;
+invariant out vec4 in_Color;
+invariant out vec4 position;
+invariant out vec2 in_UV;
+void main() 
+{
+	vec4 pos = vec4( in_Position.x, in_Position.y * abs(sin(time)), in_Position.z, 1.0);
+    gl_Position = (ProjectionMatrix * (ViewMatrix * ModelMatrix)) * pos;
+	position = pos;
+	in_Color = dgl_Color;
+	in_UV = vertexUV;
+}
